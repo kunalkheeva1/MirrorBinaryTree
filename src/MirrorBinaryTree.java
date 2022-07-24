f8b105488433c56915d5658af131a466745e4a76
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-23 20:29:13 -0700

    coded a TreeNode class

--- src/MirrorBinaryTree.java
+++ src/MirrorBinaryTree.java
@@ -1,5 +1,18 @@
+class TreeNode{
+    int data;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int data, TreeNode left, TreeNode right)
+    {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+
+    }}
+
+
 public class MirrorBinaryTree {
-    
+
 
 
     public static void main(String[] args) {
